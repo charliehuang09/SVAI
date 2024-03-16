ab7653f60bc2ee2506384ee15eb1fcffa06aa3ce
charliehuang09 <charliehuang09@gmail.com>
2024-03-16 16:49:57 -0700

    traning loop
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

--- train.py
+++ train.py
@@ -1,10 +1,29 @@
 import torch
 from dataset import Dataset
 from model import Model
+from tqdm import trange
+from torch.utils.data import DataLoader
+from torchsummary import summary
 
 def main():
-    dataset = Dataset()
     model = Model()
+
+    summary(model, (1, 7))
+
+    dataset = Dataset()
+    dataloader = DataLoader(dataset, batch_size=64)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    loss_fn = torch.nn.MSELoss()
+
+    for epoch in trange(30):
+        for batch in dataloader:
+            x, y = batch
+            optimizer.zero_grad()
+            outputs = model(x)
+            loss = loss_fn(outputs[:, 0], y)
+            loss.backward()
+            optimizer.step()
 
 if __name__=='__main__':
     main()

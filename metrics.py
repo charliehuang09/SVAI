b914676df1e32e1b885de39b88802bf37b55ccf0
charliehuang09 <charliehuang09@gmail.com>
2024-08-10 08:14:21 -0700

    Seperate metrics.py to metrics.py and plot.py
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

--- metrics.py
+++ metrics.py
@@ -22,12 +22,6 @@ import warnings
 import imageio
 
 warnings.filterwarnings("ignore")
-
-
-def clear(path):
-    filelist = [ f for f in os.listdir(path) if f.endswith(".png") ]
-    for f in filelist:
-        os.remove(os.path.join(path, f))
         
 def getConfusionMatrix(model, dataloader):
     device = config.device
@@ -82,10 +76,6 @@ def getScatterPlot(model, dataloader):
     matplotlib.use('agg')
     return plt.scatter(y_pred, y_true).get_figure()
 
-def scale(x, min, max):
-    x = ((x - min) / (max - min))
-    return x
-
 def remap(model, index=0):
     dataset = Dataset("Train", 0.8, 'Regression', True, verbose=False)
     x_min = dataset.getxmin()
@@ -137,6 +127,7 @@ def remap(model, index=0):
             map[i][j] = model(x[:, i, j])[0]
     
     return map, y
+
 def writeRemap(model, writer):
     matplotlib.use('agg')
     predsTrain = []
@@ -190,123 +181,3 @@ def writeRemap(model, writer):
     ax[1].set_title("Ground Truth")
     
     writer.add_figure("valid/RemapAverage", fig)
-    
-def writeVideo(imagePath, videoPath):
-    writer = imageio.get_writer(videoPath, fps=2)
-    for file in tqdm(sorted(glob.glob(os.path.join(imagePath, f'*.png')))):
-        im = imageio.imread(file)
-        writer.append_data(im)
-    writer.close()
-    
-def writeScatter(x, y, path):
-    x = x.flatten()
-    y = y.flatten()
-    print(x.shape, y.shape)
-    fig, ax = plt.subplots(1, 1)
-    ax.scatter(x, y)
-    ax.plot(np.linspace(0, 40, 100))
-    fig.savefig(path)
-
-def main():
-    clear('metrics/train')
-    clear('metrics/valid')
-    
-    if os.path.exists('metrics/train.mp4'):
-        os.remove('metrics/train.mp4')
-    
-    if os.path.exists('metrics/valid.mp4'):
-        os.remove('metrics/valid.mp4')
-    
-    model = torch.load('model.pt')
-    
-    predsTrain = []
-    ground_truth_Train = []
-    
-    predsValid = []
-    ground_truth_Valid = []
-    for i in trange(119):
-        map, y = remap(model, i)
-        
-        if (i <= config.train_test_split * 120):
-            predsTrain.append(map)
-            ground_truth_Train.append(y)
-        else:
-            predsValid.append(map)
-            ground_truth_Valid.append(y)
-    
-    predsTrain = np.array(predsTrain)
-    ground_truth_Train = np.array(ground_truth_Train)
-    
-    predsValid = np.array(predsValid)
-    ground_truth_Valid = np.array(ground_truth_Valid)
-    
-    preds = np.concatenate((predsTrain, predsValid))
-    ground_truth = np.concatenate((ground_truth_Train, ground_truth_Valid))
-    
-    preds = np.nanmean(np.nanmean(preds, axis=2), axis=1)
-    ground_truth = np.nanmean(np.nanmean(ground_truth, axis=2), axis=1)
-    
-    np.save('metrics/preds.npy', preds)
-    np.save('metrics/ground_truth.npy', ground_truth)
-    
-    print(predsTrain.shape, ground_truth_Train.shape)
-    print(predsValid.shape, ground_truth_Valid.shape)
-    
-    min_, max_, mean_  = min(np.nanmin(np.array((ground_truth_Train, predsTrain))), np.nanmin(np.array((ground_truth_Valid, predsValid)))), max(np.nanmax(np.array((ground_truth_Train, predsTrain))), np.nanmax(np.array((ground_truth_Valid, predsValid)))), (np.nanmean(np.array((ground_truth_Train, predsTrain))) + np.nanmean(np.array((ground_truth_Valid, predsValid))) / 2)
-    print(min_, max_, mean_)
-    
-    max_ = 25
-    
-    for i in range(len(predsTrain)):
-        fig, ax = plt.subplots(1, 3)
-        
-        #'Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Grays', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'crest', 'crest_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'flare', 'flare_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_grey', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gist_yerg', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'grey', 'hot', 'hot_r', 'hsv', 'hsv_r', 'icefire', 'icefire_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'mako', 'mako_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'rocket', 'rocket_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'vlag', 'vlag_r', 'winter', 'winter_r'
-        
-        fig.suptitle(f'Index: {i}')
-        
-        ax0img = ax[0].imshow(predsTrain[i], cmap=plt.cm.get_cmap("BrBG"), vmin=min_, vmax=max_)
-        ax[0].set_title("Predictions")
-        
-        ax1img = ax[1].imshow(ground_truth_Train[i], cmap=plt.cm.get_cmap("BrBG"), vmin=min_, vmax=max_)
-        ax[1].set_title("Ground Truth")
-        
-        plt.colorbar(ax0img, ax=ax.ravel().tolist()[:2]) 
-        
-        ax2img = ax[2].imshow(abs(ground_truth_Train[i] - predsTrain[i]), cmap=plt.cm.get_cmap("CMRmap"), vmin=min_, vmax=max_)
-        ax[2].set_title("Difference")
-        
-        plt.colorbar(ax2img, ax=ax.ravel().tolist()[2:]) 
-        
-        fig.savefig(f'metrics/train/{i}.png')
-        plt.close()
-        
-    
-    for i in range(len(predsValid)):
-        fig, ax = plt.subplots(1, 3) #tune cmap
-        
-        #'Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Grays', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'crest', 'crest_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'flare', 'flare_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_grey', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gist_yerg', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'grey', 'hot', 'hot_r', 'hsv', 'hsv_r', 'icefire', 'icefire_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'mako', 'mako_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'rocket', 'rocket_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'vlag', 'vlag_r', 'winter', 'winter_r'
-
-        fig.suptitle(f'Index: {i}')
-        
-        ax0img = ax[0].imshow(predsValid[i], cmap=plt.cm.get_cmap("BrBG"), vmin=min_, vmax=max_)
-        ax[0].set_title("Predictions")
-        
-        ax1img = ax[1].imshow(predsValid[i], cmap=plt.cm.get_cmap("BrBG"), vmin=min_, vmax=max_)
-        ax[1].set_title("Ground Truth")
-        
-        plt.colorbar(ax0img, ax=ax.ravel().tolist()[:2]) 
-        
-        ax2img = ax[2].imshow(abs(ground_truth_Valid[i] - predsValid[i]), cmap=plt.cm.get_cmap("CMRmap"), vmin=min_, vmax=max_)
-        ax[2].set_title("Difference")
-        
-        plt.colorbar(ax2img, ax=ax.ravel().tolist()[2:]) 
-        
-        fig.savefig(f'metrics/valid/{i}.png')
-        plt.close()
-    
-    writeVideo('metrics/train', 'metrics/train.mp4')
-    writeVideo('metrics/valid', 'metrics/valid.mp4')
-    
-
-if __name__=='__main__':
-    main()
